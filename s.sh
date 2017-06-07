f46b9765bf1a588896a2b46865495f4ded5602c5
Andreas Krey <a.krey@aquilius.com>
2017-06-07 23:38:54 +0200

    keep hostkeys in /data

--- s.sh
+++ s.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-mkdir /var/run/sshd
-/usr/bin/ssh-keygen -A
+mkdir -p /data/ssh
+for i in rsa dsa ecdsa ed25519; do
+  test -r /data/ssh/$i || ssh-keygen -f /data/ssh/$i -t $i -N ''
+done
+
+mkdir -p /var/run/sshd
 exec /usr/sbin/sshd -D -e
